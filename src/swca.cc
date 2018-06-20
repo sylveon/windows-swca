1c51c0ea7b86babf1af6133e21a9e3b54fcce3de
Charles Milette <me@charlesmilette.net>
2018-06-20 13:03:32 -0400

    Improve native code

--- src/swca.cc
+++ src/swca.cc
@@ -1,44 +1,44 @@
+#include <cstdint>
+#include <dwmapi.h>
 #include <node.h>
 #include <node_buffer.h>
-#include <dwmapi.h>
 
 struct ACCENTPOLICY
 {
-	int nAccentState;
-	int nFlags;
-	unsigned int nColor;
-	int nAnimationId;
+	int32_t nAccentState;
+	int32_t nFlags;
+	uint32_t nColor;
+	int32_t nAnimationId;
 };
 
 struct WINCOMPATTRDATA
 {
-	int nAttribute;
-	PVOID pData;
-	ULONG ulDataSize;
+	int32_t nAttribute;
+	void *pData;
+	uint32_t ulDataSize;
 };
 
-void swca(const v8::FunctionCallbackInfo<v8::Value>& args)
+void swca(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-	typedef BOOL(WINAPI*pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA*);
+	typedef BOOL(WINAPI *pSetWindowCompositionAttribute)(HWND, WINCOMPATTRDATA *);
 	static pSetWindowCompositionAttribute SetWindowCompositionAttribute = (pSetWindowCompositionAttribute)GetProcAddress(GetModuleHandle(TEXT("user32.dll")), "SetWindowCompositionAttribute");
 	bool returnValue = false;
 
 	if (SetWindowCompositionAttribute)
 	{
-		unsigned char* bufferData = (unsigned char*)node::Buffer::Data(args[0].As<v8::Object>());
-		HWND handle = *reinterpret_cast<HWND*>(bufferData);
+		HWND handle = *reinterpret_cast<HWND *>(node::Buffer::Data(args[0].As<v8::Object>()));
 
 		ACCENTPOLICY policy = {
-			(int)args[1]->NumberValue(),
+			static_cast<int32_t>(args[1]->NumberValue()),
 			2, // No idea what this means, but it allows to use nColor correctly.
-			(unsigned int)args[2]->NumberValue(),
+			static_cast<uint32_t>(args[2]->NumberValue()),
 			0
 		};
 
 		WINCOMPATTRDATA data = {
 			19, // WCA_ACCENT_POLICY
 			&policy,
-			sizeof(ACCENTPOLICY)
+			sizeof(policy)
 		};
 
 		returnValue = SetWindowCompositionAttribute(handle, &data);
